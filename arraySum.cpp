8dc5da759c60527076dbc2649500d1551c30feeb
Rohit Kumar Ranjan <119317461+Rohitkrranjan@users.noreply.github.com>
2024-02-11 00:15:29 +0530

    Add files via upload

---
+++ arraySum.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int arraySum(int *arr , int size)
+{
+   // base case
+    if(size==0)
+	 return 0;
+	if(size == 1)
+	 return arr[0];
+    int s=arraySum(arr+1,size-1);
+    int sum = arr[0] + s;
+    return sum;	
+}
+
+int main(){
+	int n;
+	cin>>n;
+	int *arr = new int[n];
+	
+   // for input in array	
+    for(int i=0;i<n;i++)
+    cin>>arr[i];
+    
+    int ans = arraySum(arr,n);
+    
+    cout<<"Sum of array is = "<<ans;
+}
